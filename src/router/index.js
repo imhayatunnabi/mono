3065da341cae019cebc04fdbd8f1122ebdd58941
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2023-07-29 14:57:10 +0600

    update the templating

--- src/router/index.js
+++ src/router/index.js
@@ -5,9 +5,9 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'DashboardComponent',
       component: () => import('../views/backend/dashboard/DashboardComponent.vue')
-    },
+    }
   ]
 })
 
